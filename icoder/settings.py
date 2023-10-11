448e73c6802e4bdcf2ed2a2ade893bd7bc72da6c
mahesh191201 <rskmahesh5000@gmail.com>
2023-10-11 23:12:48 +0530

    tenth commit

--- icoder/settings.py
+++ icoder/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_ROOT=[
+STATICFILES_DIR=[
     os.path.join(BASE_DIR, 'static')
 ]
 
