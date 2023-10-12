5e463bc2078dfd78c1ce7b9ea1b5207c83f2cd5d
mahesh191201 <rskmahesh5000@gmail.com>
2023-10-12 19:43:18 +0530

    thirteen commit

--- icoder/settings.py
+++ icoder/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'ckeditor',
     'django_social_share',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 
    
 ]
@@ -59,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
    
 ]
 
